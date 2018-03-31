af1a3484b8acd413206919b3fb9df580bef9bf1d
Brent Pierce <bxp84590@ucmo.edu>
2018-03-31 10:02:39 -0500

    Added security config classes

---
+++ src/main/java/security/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+/*
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {RootConfiguration.class};
+    }*/
+}
