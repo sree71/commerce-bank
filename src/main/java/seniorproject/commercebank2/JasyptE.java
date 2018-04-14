f1f2204ffa92a4e76c9179c079b4d2057f5c75fd
Brent Pierce <bxp84590@ucmo.edu>
2018-04-14 12:16:41 -0500

    Commented out loginstyle.css until can be fixed. Changed jasypt version.

--- src/main/java/seniorproject/commercebank2/JasyptE.java
+++ src/main/java/seniorproject/commercebank2/JasyptE.java
@@ -5,6 +5,7 @@ import org.jasypt.util.text.BasicTextEncryptor;
 public class JasyptE{
 
     public String encrypt(String plainText){
+
         BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
         String encryptedText = textEncryptor.encrypt(plainText);
         return encryptedText;
